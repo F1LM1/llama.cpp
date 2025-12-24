3fe9ece4b55049100d68573781941375133ac5f3
samuel <samueloliveira32df@gmail.com>
2025-12-24 11:09:49 -0300

    batch: allow position reuse for MTP warmup

--- src/llama-batch.cpp
+++ src/llama-batch.cpp
@@ -301,16 +301,23 @@ bool llama_batch_allocr::init(
                     ok = false;
                 }
 
-            // if (!ok) {
-            //     LLAMA_LOG_ERROR(
-            //             "%s: the tokens of sequence %d in the input batch have inconsistent sequence positions:\n"
-            //             " - the last position stored in the memory module of the context (i.e. the KV cache) for sequence %d is X = %d\n"
-            //             " - the tokens for sequence %d in the input batch have a starting position of Y = %d\n"
-            //             " it is required that the sequence positions remain consecutive: Y = X + 1\n",
-            //             __func__, s, s, p0, s, seq_pos_min(s));
+                // MTP Update kv-cache reuse the same position from before.
+                if (batch.mtp_params.op_type == MTP_OP_WARMUP) {
+                    if (seq_pos_min(s) <= p0 + 1) { 
+                        ok = true; 
+                    }
+                }
 
-            //     return false;
-            // }
+            if (!ok) {
+                LLAMA_LOG_ERROR(
+                        "%s: the tokens of sequence %d in the input batch have inconsistent sequence positions:\n"
+                        " - the last position stored in the memory module of the context (i.e. the KV cache) for sequence %d is X = %d\n"
+                        " - the tokens for sequence %d in the input batch have a starting position of Y = %d\n"
+                        " it is required that the sequence positions remain consecutive: Y = X + 1\n",
+                        __func__, s, s, p0, s, seq_pos_min(s));
+
+                return false;
+            }
         }
 
             if (seq_pos_max(s) - seq_pos_min(s) + 1 > (int) seq_pos[s].size()) {
